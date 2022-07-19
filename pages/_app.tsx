8237c5c6a277f39786dc57924be22e80cff5e6d3
Miles Yucht <mgyucht@gmail.com>
2022-07-19 15:24:52 -0400

    Add footer with links to social media (#5)
    * Add footer with links to social media
    
    * add transition and hover effect for footer icons
    
    * fix lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from "next/app";
 import "../styles/global.css";
 
+// For Font Awesome, see https://fontawesome.com/docs/web/use-with/react/use-with
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
